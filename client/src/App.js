87d768656ad760f7a9c6e844819333bb91d19e6a
octaviolafourcade <octavio.lafourcade@gmail.com>
2022-01-16 18:00:48 -0500

    [ ADD ] create Error, Register, Dashboard pages [ ADD ] Create index.js for pages folder [ ADD ] Import all pages into index.js and export them together [ ADD ] Import pages from index.js in App.js and modify Route element

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,15 @@
 
-import Landing from "./pages/Landing"
+import { Landing, Dashboard, Error, Register } from "./pages"
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter className="App">
-      <nav>
-        <Link to='/'>Dashboard</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/landing'>Landing</Link>
-      </nav>
       <Routes>
-        <Route path='/' element={<div>Dashboard</div>}/>
-        <Route path='/register' element={<div>Register</div>}/>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/register' element={<Register/>}/>
         <Route path='/landing' element={<Landing/>}/>
-        <Route path='*' element={<div>Error</div>}/>
+        <Route path='*' element={<Error/>}/>
         
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div>
+            Dashobard!
+        </div>
+    )
+}
+
+export default Dashboard
