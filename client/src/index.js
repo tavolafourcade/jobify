30d41769e37d391ce55d7d3aff709bd3a981875d
tavolafourcade <tavolafourcade@gmail.com>
2022-02-01 22:38:12 -0500

    [ ADD ] AppProvider setup in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import 'normalize.css'
 import './index.css';
 import App from './App';
+import { AppProvider } from './context/appContext'
 
 ReactDOM.render(
-    <App />,
+  <AppProvider>
+    <App />
+  </AppProvider>
+    ,
   document.getElementById('root')
 );
