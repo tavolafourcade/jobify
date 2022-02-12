6bf09070110ebe35a2186ba679bc711953f8ef4c
tavolafourcade <tavolafourcade@gmail.com>
2022-02-12 15:44:06 -0500

    [ ADD ] Not found middleware

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js'
 const app = express()
 
 // Set up a dummy route
 app.get('/', (req, res) => {
 	res.send('Welcome!')
 })
+
+// Using the middleware to look for all Http methods and Routes.
+// If none are found 
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
