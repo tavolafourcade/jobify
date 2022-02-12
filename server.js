66c172c09eed0b501f25b66c087e3b38210770b8
tavolafourcade <tavolafourcade@gmail.com>
2022-02-12 17:48:13 -0500

    [ ADD ] Error handler middleware

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from 'express'
-import notFoundMiddleware from './middleware/not-found.js'
 const app = express()
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 // Set up a dummy route
 app.get('/', (req, res) => {
+	throw new Error('Error!!!')
 	res.send('Welcome!')
 })
 
 // Using the middleware to look for all Http methods and Routes.
 // If none are found 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
