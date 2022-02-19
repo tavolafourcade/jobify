b4f0660cc0de080b23e7445e714590c7cd7771f8
tavolafourcade <tavolafourcade@gmail.com>
2022-02-18 23:28:38 -0500

    [ ADD ] setup the structure for the authController and Routes in server.js

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
+
+//db and authenticateUser
 import connectDB from './db/connect.js'
+
+//routers
+import authRouter from './routes/authRoutes.js'
 
 //middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
+//Making the JSON data available in the controller
+app.use(express.json())
 // Set up a dummy route
 app.get('/', (req, res) => {
 	// throw new Error('Error!!!')
 	res.send('Welcome!')
 })
+
+app.use('/api/v1/auth', authRouter)
 
 // Using the middleware to look for all Http methods and Routes.
 // If none are found 
