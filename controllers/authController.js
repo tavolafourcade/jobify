52a743f350334a04276703403f6963c6c2fe10b5
tavolafourcade <tavolafourcade@gmail.com>
2022-02-27 00:04:21 -0500

    [ ADD ] Check for empty values in the Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,14 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
 const register = async (req, res, next) => {
+    // CHECK FOR EMPTY VALUES IN THE CONTROLLER
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password) {
+        throw new Error('Please provide all values ')
+    }
     // In req.body all the values will be located, and send them into MongoDB
-    const user = await User.create(req.body)
+    const user = await User.create({name, email, password})
     res.status(StatusCodes.CREATED).json({user})
 }
 
