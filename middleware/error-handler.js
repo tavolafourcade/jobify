9567a735108edc971ff0af57502b5eaabcc216d6
tavolafourcade <tavolafourcade@gmail.com>
2022-02-26 22:49:37 -0500

    [ ADD ] Unique field error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -12,8 +12,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         .map(item => item.message)
         .join(',')
     }
-        res.status(defaultError.statusCode).json({msg:defaultError.msg})
-        // res.status(defaultError.statusCode).json({msg: defaultError.msg})
+    // If an error exist and the value for the code property is 11000
+    if (err.code && err.code === 11000){
+        // The statusCode should be 400
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+
+        // Setting up the message
+        defaultError.msg = `${Object.keys(err.keyValue)} Field has to be unique`
+    }
+        // res.status(defaultError.statusCode).json({msg:err})
+        res.status(defaultError.statusCode).json({msg: defaultError.msg})
 
 }
 
