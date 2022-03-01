22f244653f8ca3c412178faaf9fad00487d9d546
tavolafourcade <tavolafourcade@gmail.com>
2022-02-28 23:25:11 -0500

    [ ADD ]customAPIError to receive a custom message and hardcode a status 400 error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
 const register = async (req, res, next) => {
+
+    class customAPIError extends Error {
+        constructor(message) {
+            super(message)
+            this.statusCode = StatusCodes.BAD_REQUEST
+        }
+    }
     // CHECK FOR EMPTY VALUES IN THE CONTROLLER
     const {name, email, password} = req.body
 
     if(!name || !email || !password) {
-        throw new Error('Please provide all values ')
+        throw new customAPIError('Please provide all values ')
     }
     // In req.body all the values will be located, and send them into MongoDB
     const user = await User.create({name, email, password})
