767dbad5044877dd56219874cc663619105639dc
tavolafourcade <tavolafourcade@gmail.com>
2022-03-01 23:43:25 -0500

    [ ADD ] Check for Duplicate Email in Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,6 +9,12 @@ const register = async (req, res, next) => {
     if(!name || !email || !password) {
         throw new BadRequestError('Please provide all values ')
     }
+
+    //It will look for the user with this specific email
+    const userAlreadyExists = await User.findOne({email})
+    if(userAlreadyExists){
+        throw new BadRequestError('Email already in use')
+    }
     // In req.body all the values will be located, and send them into MongoDB
     const user = await User.create({name, email, password})
     res.status(StatusCodes.CREATED).json({user})
