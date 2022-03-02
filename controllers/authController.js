9775c69d69da5d0d7ce386d9729cd5beeef316e6
tavolafourcade <tavolafourcade@gmail.com>
2022-03-01 23:26:49 -0500

    [ ADD ] Refactor errors into their own files

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,6 @@
-import User from "../models/User.js"
-import { StatusCodes } from "http-status-codes"
-class CustomAPIError extends Error {
-    constructor(message) {
-        super(message)
-    }
-}
-
-class BadRequestError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.BAD_REQUEST
-    }
-}
-
-class NotFoundError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.NOT_FOUND
-    }
-}
+import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
+import {BadRequestError} from '../errors/index.js'
 
 const register = async (req, res, next) => {
     // CHECK FOR EMPTY VALUES IN THE CONTROLLER
