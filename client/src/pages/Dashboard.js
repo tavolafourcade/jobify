cd397af48d9327dfc95da5e6d6928f582c3fbf84
tavolafourcade <tavolafourcade@gmail.com>
2022-03-15 22:38:25 -0500

    [ ADD ] Trying a fetch error (CORS) due both frontend application and server dont communicate each other

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+
 const Dashboard = () => {
-    return (
-        <div>
-            Dashobard PAge!
-        </div>
-    )
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/')
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+  return <h1>Dashboard Page!</h1>
 }
 
 export default Dashboard
