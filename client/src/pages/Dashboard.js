7241ffb1823e73ae177fe11452e45e97e3444175
tavolafourcade <tavolafourcade@gmail.com>
2022-04-08 19:15:22 -0500

    [ REMOVE ] Comment Dashboard example that is not being used anymore

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,20 @@
 import { useEffect } from 'react'
 
 const Dashboard = () => {
-  const fetchData = async () => {
-    try {
-      // const response = await fetch('/data.json')
-      const response = await fetch('/api/v1')
-      const data = await response.json()
-      console.log(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  useEffect(()=>{
-    fetchData()
-  },[])
+  // EXAMPLE that is not used anymore
+  // const fetchData = async () => {
+  //   try {
+  //     // const response = await fetch('/data.json')
+  //     const response = await fetch('/api/v1')
+  //     const data = await response.json()
+  //     console.log(data)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  // useEffect(()=>{
+  //   fetchData()
+  // },[])
   return <h1>Dashboard Page!</h1>
 }
 
