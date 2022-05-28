23170384933f3470dbd63deb9b3d6756237d7da4
tavolafourcade <tavolafourcade@gmail.com>
2022-05-27 20:33:57 -0500

    [ DELETE ] Previous Dashboard dummy page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import { Landing, Dashboard, Error, Register } from "./pages"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Landing, Error, Register } from './pages'
 
 function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
-        <Route path='/' element={<Dashboard/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/landing' element={<Landing/>} />
-        <Route path='*' element={<Error/>} />
-        
+        <Route path="/" element={<div>Dashboard!</div>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,6 +1,5 @@
-import Landing from "./Landing"
-import Dashboard from "./Dashboard"
-import Error from "./Error"
-import Register from "./Register"
+import Landing from './Landing'
+import Error from './Error'
+import Register from './Register'
 
-export { Landing, Dashboard, Error, Register }
+export { Landing, Error, Register }
