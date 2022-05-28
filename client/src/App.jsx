028bffb6f29e92b0600ab3a2ee5418d5ae0a7f1a
tavolafourcade <tavolafourcade@gmail.com>
2022-05-28 16:37:12 -0500

    [ ADD ] ProtectedRoute to navigate to /landing if there is not user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Landing, Error, Register } from './pages'
+import {
+  Landing, Error, Register, ProtectedRoute,
+} from './pages'
 import {
   AddJob, AllJobs, Profile, SharedLayout, Stats,
 } from './pages/dashboard'
@@ -8,7 +10,14 @@ function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={(
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          )}
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,8 @@
 import Landing from './Landing'
 import Error from './Error'
 import Register from './Register'
+import ProtectedRoute from './ProtectedRoute'
 
-export { Landing, Error, Register }
+export {
+  Landing, Error, Register, ProtectedRoute,
+}
