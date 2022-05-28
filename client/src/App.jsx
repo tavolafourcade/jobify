6ca751188b266fd05450bce29c8fa542272db0a2
tavolafourcade <tavolafourcade@gmail.com>
2022-05-27 23:05:25 -0500

    [ ADD ] New routing path for nested pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React, { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Landing, Error, Register } from './pages'
+import {
+  AddJob, AllJobs, Profile, SharedLayout, Stats,
+} from './pages/dashboard'
 
 function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
-        <Route path="/" element={<div>Dashboard!</div>} />
+        <Route path="/">
+          <Route path="addjob" element={<AddJob />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="stats" element={<Stats />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
