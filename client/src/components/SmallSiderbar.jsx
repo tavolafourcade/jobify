b2764a980684cbe348cd5df4bc0de95708e2d213
tavolafourcade <tavolafourcade@gmail.com>
2022-05-28 17:08:26 -0500

    [ ADD ] Components to be used in SharedLayout

---
+++ client/src/components/SmallSiderbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/SmallSidebar'
+
+function SmallSiderbar() {
+  return (
+    <Wrapper>
+      <div>SmallSiderbar</div>
+    </Wrapper>
+  )
+}
+
+export default SmallSiderbar
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,5 +1,10 @@
 import Logo from './Logo'
 import FormRow from './FormRow'
 import Alert from './Alert'
+import Navbar from './Navbar'
+import SmallSiderbar from './SmallSiderbar'
+import BigSidebar from './BigSidebar'
 
-export { Logo, FormRow, Alert }
+export {
+  Logo, FormRow, Alert, Navbar, SmallSiderbar, BigSidebar,
+}
