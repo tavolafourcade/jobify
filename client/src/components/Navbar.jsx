5611f883f4531ca4c83a5483dcc385e8f3451d5d
tavolafourcade <tavolafourcade@gmail.com>
2022-05-28 19:21:48 -0500

    [ ADD ] Navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
-import { FaHome } from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
+// import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 import Wrapper from '../assets/wrappers/Navbar'
 
 function Navbar() {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log('show/hide dropdown')}
+          >
+            <FaUserCircle />
+            Octavio
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log('logout user')}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
