3a33b765e6dc78a2c15de5ddcf9c4a695c59ff82
tavolafourcade <tavolafourcade@gmail.com>
2022-05-29 13:54:54 -0500

    [ ADD ] Click outside to close logoutUser menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
 import { useAppContext } from '../context/appContext'
 import Logo from './Logo'
@@ -7,7 +7,23 @@ import Wrapper from '../assets/wrappers/Navbar'
 function Navbar() {
   const { user, logoutUser, toggleSidebar } = useAppContext()
   const [ showLogout, setShowLogout ] = useState(false)
+  const logoutRef = useRef()
   console.log('SHOWLOGOUT', showLogout)
+
+  useEffect(
+    () => {
+      document.addEventListener('mousedown', (e) => {
+        if (!logoutRef.current.contains(e.target)) { setShowLogout(false) }
+      })
+      // const closeLogoutDropdown = (e) => {
+      //   if (e.path[0].tagName !== 'BUTTON') {
+      //     setShowLogout(false)
+      //   }
+      // }
+    },
+    [ ],
+  )
+
   return (
     <Wrapper>
       <div className="nav-center">
@@ -22,7 +38,7 @@ function Navbar() {
           <Logo />
           <h3 className="logo-text">Dashboard</h3>
         </div>
-        <div className="btn-container">
+        <div className="btn-container" ref={logoutRef}>
           <button
             type="button"
             className="btn"
@@ -32,7 +48,7 @@ function Navbar() {
             {user?.name}
             <FaCaretDown />
           </button>
-          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'} >
             <button
               type="button"
               className="dropdown-btn"
