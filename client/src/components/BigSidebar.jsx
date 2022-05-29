0e0f54bf9a99a38768a7783a8d032047c6adfafd
tavolafourcade <tavolafourcade@gmail.com>
2022-05-29 13:04:10 -0500

    [ ADD ] Implementation of the Big side bar

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/BigSidebar'
+import { useAppContext } from '../context/appContext'
+import NavLinks from './NavLinks'
+import Logo from './Logo'
 
 function BigSidebar() {
+  const { showSidebar, toggleSidebar } = useAppContext()
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'} >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   )
 }
