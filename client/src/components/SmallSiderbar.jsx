da5d61666c6a13e6ab11adebf2a38afa78888cd5
tavolafourcade <tavolafourcade@gmail.com>
2022-05-29 11:41:35 -0500

    [ ADD ] Small Sidebar setup

--- client/src/components/SmallSiderbar.jsx
+++ client/src/components/SmallSiderbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { useAppContext } from '../context/appContext'
+import links from '../utils/links'
+import Logo from './Logo'
 
 function SmallSiderbar() {
   return (
     <Wrapper>
-      <div>SmallSiderbar</div>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type="button" className="close-btn" onClick={() => console.log('TOGGLE SIDEBAR')}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
