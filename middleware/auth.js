50020707efa182fa32875c670f6da63713afd3e6
tavolafourcade <tavolafourcade@gmail.com>
2022-05-31 18:26:56 -0500

    [ ADD ] UnAuthenticatedError validation if authHeader is missing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,14 @@
+// eslint-disable-next-line import/extensions
+import { UnAuthenticatedError } from '../errors/index.js';
+
 const auth = async (req, res, next) => {
-  const { headers } = req;
+  // const { headers } = req;
   const authHeader = req.headers.authorization;
 
-  console.log(headers, 'headers');
+  // console.log(headers, 'headers');
+  if (!authHeader) {
+    throw new UnAuthenticatedError('Authentication Invalid');
+  }
   console.log(authHeader, 'authHeader');
 
   next();
