7d8f1ba3d51c14c89fc5d4702cdf48e91466e1d6
tavolafourcade <tavolafourcade@gmail.com>
2022-06-18 22:56:23 -0500

    [ ADD ] Edit Job controller for updating a job

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 import { StatusCodes } from 'http-status-codes';
 import Job from '../models/Job.js';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 // UnAuthenticatedError
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -25,7 +25,26 @@ const getAllJobs = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send('updateJob');
+  const { id: jobId } = req.params;
+  const { company, position } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  // check permission
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 const showStats = async (req, res) => {
