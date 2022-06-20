cd5ab53eadeaf97554fa33d2b5f051c66d30e208
tavolafourcade <tavolafourcade@gmail.com>
2022-06-20 18:08:07 -0500

    [ ADD ] deleteJob controller in the backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -15,7 +15,18 @@ const createJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('deleteJob');
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job deleted' });
 };
 
 const getAllJobs = async (req, res) => {
