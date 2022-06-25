cb66610d4ed682ca3a84e2dfb2d683988318bb41
tavolafourcade <tavolafourcade@gmail.com>
2022-06-25 14:08:38 -0500

    [ ADD ] Stat items implementation

--- client/src/components/StatItem.jsx
+++ client/src/components/StatItem.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/StatItem'
 
-function StatItem() {
+function StatItem({
+  title, count, icon, color, bcg,
+}) {
   return (
-    <div>StatItem</div>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <div className="icon">{icon}</div>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
   )
 }
 
