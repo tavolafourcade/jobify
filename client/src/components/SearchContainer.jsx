2d2b2b14de697f4023c309b228a605fe80436ba4
tavolafourcade <tavolafourcade@gmail.com>
2022-06-26 16:58:44 -0500

    [ ADD ] SearchContainer initial setup

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { FormRow, FormRowSelect } from '.'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/SearchContainer'
 
 function SearchContainer() {
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext()
+
+  const handleSearch = (e) => {
+    if (isLoading) return // prevent search while loading
+    handleChange({ name: e.target.name, value: e.target.value })
+  }
   return (
-    <div>Search component</div>
+    <Wrapper>
+      <form className="form">
+        <h4>Search form</h4>
+        {/* Search position */}
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="search" // name of the input must exactly match the state name
+            value={search}
+            handleChange={handleSearch}
+          />
+          {/* Rest of inputs */}
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 
