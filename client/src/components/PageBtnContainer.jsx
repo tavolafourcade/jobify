b42e35e758e33286dc99ed804a86acf4860cf7c2
tavolafourcade <tavolafourcade@gmail.com>
2022-06-26 21:09:02 -0500

    [ ADD ] Button container structure for pagination

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/PageBtnContainer'
 
 function PageBtnContainer() {
+  const { numOfPages, page } = useAppContext()
+
+  const prevPage = () => {
+    console.log('prevPage')
+  }
+
+  const nextPage = () => {
+    console.log('nextPage')
+  }
   return (
-    <div>PageBtnContainer</div>
+    <Wrapper>
+      <button type="button" className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">buttons</div>
+      <button type="button" className="next-btn" onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
   )
 }
 
