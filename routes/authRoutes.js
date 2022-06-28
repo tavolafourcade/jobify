19a75806e5bdfd527267194f6d933d76c8065bf0
tavolafourcade <tavolafourcade@gmail.com>
2022-06-28 18:17:11 -0500

    [ ADD ] Limit the number of requests for login and register with rateLimiter

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,21 @@
 import express from 'express';
+
+import rateLimiter from 'express-rate-limit';
 // eslint-disable-next-line import/extensions
 import { register, login, updateUser } from '../controllers/authController.js';
 // eslint-disable-next-line import/extensions
 import authenticateUser from '../middleware/auth.js';
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // limit each IP to 100 requests per windowMs
+  message: 'Too many accounts created from this IP, please try again after 15 minutes',
+});
 // eslint-disable-next-line import/extensions
 const router = express.Router();
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/register').post(apiLimiter, register);
+router.route('/login').post(apiLimiter, login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
 export default router;
